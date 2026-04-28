439ae561173877387d50829b90695933d6f891d3
KINJO Kazusa <e245735@ie.u-ryukyu.ac.jp>
2026-04-28 11:27:54 +0900

    ex1.10: テスト用データセットに対する予測

--- src/ex4/learn1.py
+++ src/ex4/learn1.py
@@ -1,6 +1,6 @@
 import datasets
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split, cross_val_score, KFold
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
 
@@ -9,3 +9,9 @@ X_train, X_test, y_train, y_test = train_test_split(df[["観測点"]], df["観�
 
 model = LinearRegression()
 model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(f"Mean Squared Error: {mse:.2f}")
+print(f"Coefficient of Determination: {r2:.2f}")
