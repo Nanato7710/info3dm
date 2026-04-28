89e3ed89cf492fa2b7e1f4b4f9a015d23917f5ac
KINJO Kazusa <e245735@ie.u-ryukyu.ac.jp>
2026-04-28 10:52:19 +0900

    ex1.3: ノイズの付与

--- src/ex4/datasets.py
+++ src/ex4/datasets.py
@@ -14,6 +14,11 @@ def get_true_function_data(n_samples: int = 20, random_seed: int = 0):
     y = true_function(x)
     return x, y
 
+def add_noise(y: np.ndarray, noise_mean: float = 0, noise_std: float = 2.0, random_seed: int = 0) -> np.ndarray:
+    np.random.seed(random_seed)
+    noise = np.random.normal(noise_mean, noise_std, size=y.shape)
+    return y + noise
+
 if __name__ == "__main__":
     import matplotlib.pyplot as plt
     import pandas as pd
@@ -24,12 +29,24 @@ if __name__ == "__main__":
     data_x, data_y = get_true_function_data(n_samples=20, random_seed=0)
     df = pd.DataFrame({"観測点": data_x, "真値": data_y})
 
-    plt.plot(x, y, color="blue", label="True Function")
+    noisy_y = add_noise(data_y, noise_mean=0, noise_std=2.0, random_seed=0)
+    df["観測値"] = noisy_y
+
+    plt.title("True Function")
     plt.xlabel("x")
     plt.ylabel("y")
-    plt.title("True Function")
     plt.grid()
+
+    plt.plot(x, y, color="blue", label="True Function", )
+    plt.legend()
     plt.savefig("ex1.1.png")
-    plt.scatter(df["観測点"], df["真値"], color="red", label="Observations")
+
+    plt.scatter(df["観測点"], df["真値"], color="red", label="True Values")
+    plt.legend()
     plt.savefig("ex1.2.png")
+
+    plt.scatter(df["観測点"], df["観測値"], color="green", label="Noisy Observations")
+    plt.legend()
+    plt.savefig("ex1.3.png")
+
     plt.show()
